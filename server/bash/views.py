ed5860cb81293bca96fbaf3f51d1c44aec0e64a2
Oriol López-Doriga <orildsaga@gmail.com>
2023-04-26 12:44:42 +0200

    bash command

--- server/bash/views.py
+++ server/bash/views.py
@@ -175,8 +175,8 @@ def return_bash(value1, value2, value3, value4, value5, value6, value7, value8):
         
     string = value1 + ' ' + value2 + ' ' + value3+ ' ' + value4 + ' ' + value5 + ' ' + value7 + ' ' + value6 + ' ' + value8
 
-    bash = subprocess.check_output(['cd', '/data/boxes/beacon-BED-based', '&&', 'bash', 'exec-MAIN.bash', string], shell=True)
-    #bash = subprocess.check_output(['cd', '/server/angelweb', 'ls'], shell=True)
+    #bash = subprocess.check_output(['cd', '/data/boxes/beacon-BED-based', '&&', 'bash', 'exec-MAIN.bash', string], shell=True)
+    bash = subprocess.check_output(['cd', '/data/boxes/beacon-BED-based', '&&', 'ls'], shell=True)
     #bash = subprocess.check_output(['cd', '/data/boxes/beacon-BED-based'], shell=True)
 
 
