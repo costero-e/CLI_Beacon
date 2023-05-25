79d863954d066e1b7d7527e192162ce71feea894
Oriol López-Doriga <orildsaga@gmail.com>
2023-05-25 16:10:50 +0200

    implemented Bad Request

--- server/bash/views.py
+++ server/bash/views.py
@@ -3,7 +3,10 @@ from django.views.generic import TemplateView
 import subprocess
 from angelweb.forms import BamForm, BamFormTrue, BamFormFalse
 import time
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponseBadRequest
+import logging
+
+LOG = logging.getLogger(__name__)
 
 def return_string(value1, value2, value3, value4, value5, value6, value7, value8):
     if isinstance(value1, list):
@@ -90,6 +93,77 @@ def return_string(value1, value2, value3, value4, value5, value6, value7, value8
 
 
     return string
+
+def return_list(value1, value2, value3, value4, value5, value6, value7, value8):
+    if isinstance(value1, list):
+        for value in value1:
+            value1 = str(value)
+    elif isinstance(value1, int):
+        value = str(value)
+        value1 = value
+    else:
+        value1 = str(value1)
+    if isinstance(value2, list):
+        for value in value2:
+            value2 = str(value)
+    elif isinstance(value2, int):
+        value = str(value)
+        value2 = value
+    else:
+        value2 = str(value2)
+    if isinstance(value3, list):
+        for value in value3:
+            value3 = str(value)
+    elif isinstance(value3, int):
+        value = str(value3)
+        value3 = value
+    else:
+        value3 = str(value3)
+    if isinstance(value4, list):
+        for value in value4:
+            value4 = str(value)
+    elif isinstance(value4, int):
+        value = str(value4)
+        value4 = value
+    elif value4 == None:
+        value4 = ''
+    if isinstance(value5, list):
+        for value in value5:
+            value5 = str(value)
+            value5 = "'" + value5 + "'"
+    elif isinstance(value5, int):
+        value = str(value)
+        value5 = value
+    else:
+        value5 = str(value5)
+    if isinstance(value7, list):
+        for value in value7:
+            value7 = str(value)
+    elif isinstance(value7, int):
+        value = str(value)
+        value7 = value
+    else:
+        value7 = str(value7)
+    if value6 == True:
+        value6 = 'LIFTOVER'
+    elif value6 == False:
+        value6 = ''
+    if value8 == True:
+        value8 = 'PUBLIC'
+    elif value8 == False:
+        value8 = ''
+
+    string_list = []
+    string_list.append(value1)
+    string_list.append(value2)
+    string_list.append(value3)
+    string_list.append(value4)
+    string_list.append(value5)
+    string_list.append(value7)
+    string_list.append(value6)
+    string_list.append(value8)
+
+    return string_list
 
 def return_bash(value1, value2, value3, value4, value5, value6, value7, value8):
     if isinstance(value1, list):
@@ -213,8 +287,15 @@ def bash_view(request):
 
             return render(request, 'base.html', context)
             '''
+            if form.cleaned_data['region'] != None and form.cleaned_data['mutated_allele'] != '':
+                get_string='?reference={}&chromosome={}&start={}&region={}&mutated_allele={}&answer_type={}&liftover={}&public={}'.format(form.cleaned_data['reference'], form.cleaned_data['chromosome'], form.cleaned_data['start'], form.cleaned_data['region'], form.cleaned_data['mutated_allele'],form.cleaned_data['answer_type'], form.cleaned_data['liftover'], form.cleaned_data['public'])
+            elif form.cleaned_data['region'] != None:
+                get_string='?reference={}&chromosome={}&start={}&region={}&answer_type={}&liftover={}&public={}'.format(form.cleaned_data['reference'], form.cleaned_data['chromosome'], form.cleaned_data['start'], form.cleaned_data['region'],form.cleaned_data['answer_type'], form.cleaned_data['liftover'], form.cleaned_data['public'])
+            elif form.cleaned_data['mutated_allele'] != '':
+                get_string='?reference={}&chromosome={}&start={}&mutated_allele={}&answer_type={}&liftover={}&public={}'.format(form.cleaned_data['reference'], form.cleaned_data['chromosome'], form.cleaned_data['start'], form.cleaned_data['mutated_allele'],form.cleaned_data['answer_type'], form.cleaned_data['liftover'], form.cleaned_data['public'])
+            else:
+                get_string='?reference={}&chromosome={}&start={}&answer_type={}&liftover={}&public={}'.format(form.cleaned_data['reference'], form.cleaned_data['chromosome'], form.cleaned_data['start'],form.cleaned_data['answer_type'], form.cleaned_data['liftover'], form.cleaned_data['public'])
 
-            get_string='?reference={}&chromosome={}&start={}&answer_type={}'.format(form.cleaned_data['reference'], form.cleaned_data['chromosome'], form.cleaned_data['start'], form.cleaned_data['answer_type'])
 
             return HttpResponseRedirect('/' + get_string)
         
@@ -259,6 +340,53 @@ def bash_view(request):
                     params['public']=False
             except Exception:
                 params['public']=False
+
+            listin = return_list(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], params['public'])
+            references=['37','38']
+            chromosomess=[str(x) for x in range(1, 23)] + ["X", "Y", "MT"]
+            answer_types=['BOOL','COUNT']
+            liftovers=['LIFTOVER', '']
+            publics=['PUBLIC', '']
+            
+            LOG.debug(listin)
+            print(listin)
+            
+            if listin[0] not in references:
+                return HttpResponseBadRequest('Bad Request')
+            if listin[1] not in chromosomess:
+                return HttpResponseBadRequest('Bad Request')
+            
+            if listin[2] == '':
+                pass
+            else:
+                try:
+                    if int(listin[2]) <= 99999999999:
+                        pass
+                    else:
+                        return HttpResponseBadRequest('Bad Request')
+                except Exception:
+                        return HttpResponseBadRequest('Bad Request')
+            
+            if listin[3] == '':
+                pass
+            else:
+                try:
+                    if listin[3] <= 10000:
+                        pass
+                    else:
+                        return HttpResponseBadRequest('Bad Request')
+                except Exception:
+                    return HttpResponseBadRequest('Bad Request')
+            
+            if listin[5] not in answer_types:
+                return HttpResponseBadRequest('Bad Request')
+            
+            if listin[6] not in liftovers:
+                return HttpResponseBadRequest('Bad Request')
+            if listin[7] not in publics:
+                return HttpResponseBadRequest('Bad Request')
+
+                
 
             context = {
                     'string': return_string(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], params['public']),
@@ -350,6 +478,53 @@ def bash_true_view(request):
             except Exception:
                 params['public']=False
 
+            listin = return_list(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], params['public'])
+            references=['37','38']
+            chromosomess=[str(x) for x in range(1, 23)] + ["X", "Y", "MT"]
+            answer_types=['BOOL','COUNT']
+            liftovers=['LIFTOVER', '']
+            publics=['PUBLIC', '']
+            
+            LOG.debug(listin)
+            print(listin)
+            
+            if listin[0] not in references:
+                return HttpResponseBadRequest('Bad Request')
+            if listin[1] not in chromosomess:
+                return HttpResponseBadRequest('Bad Request')
+            
+            if listin[2] == '':
+                pass
+            else:
+                try:
+                    if int(listin[2]) <= 99999999999:
+                        pass
+                    else:
+                        return HttpResponseBadRequest('Bad Request')
+                except Exception:
+                        return HttpResponseBadRequest('Bad Request')
+            
+            if listin[3] == '':
+                pass
+            else:
+                try:
+                    if listin[3] <= 10000:
+                        pass
+                    else:
+                        return HttpResponseBadRequest('Bad Request')
+                except Exception:
+                    return HttpResponseBadRequest('Bad Request')
+            
+            if listin[5] not in answer_types:
+                return HttpResponseBadRequest('Bad Request')
+            
+            if listin[6] not in liftovers:
+                return HttpResponseBadRequest('Bad Request')
+            if listin[7] not in publics:
+                return HttpResponseBadRequest('Bad Request')
+
+                
+
             context = {
                     'string': return_string(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], params['public']),
                     'bash_out': return_bash(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], params['public']),
@@ -439,6 +614,53 @@ def bash_false_view(request):
                     params['public']=False
             except Exception:
                 params['public']=False
+
+            listin = return_list(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], params['public'])
+            references=['37','38']
+            chromosomess=[str(x) for x in range(1, 23)] + ["X", "Y", "MT"]
+            answer_types=['BOOL','COUNT']
+            liftovers=['LIFTOVER', '']
+            publics=['PUBLIC', '']
+            
+            LOG.debug(listin)
+            print(listin)
+            
+            if listin[0] not in references:
+                return HttpResponseBadRequest('Bad Request')
+            if listin[1] not in chromosomess:
+                return HttpResponseBadRequest('Bad Request')
+            
+            if listin[2] == '':
+                pass
+            else:
+                try:
+                    if int(listin[2]) <= 99999999999:
+                        pass
+                    else:
+                        return HttpResponseBadRequest('Bad Request')
+                except Exception:
+                        return HttpResponseBadRequest('Bad Request')
+            
+            if listin[3] == '':
+                pass
+            else:
+                try:
+                    if listin[3] <= 10000:
+                        pass
+                    else:
+                        return HttpResponseBadRequest('Bad Request')
+                except Exception:
+                    return HttpResponseBadRequest('Bad Request')
+            
+            if listin[5] not in answer_types:
+                return HttpResponseBadRequest('Bad Request')
+            
+            if listin[6] not in liftovers:
+                return HttpResponseBadRequest('Bad Request')
+            if listin[7] not in publics:
+                return HttpResponseBadRequest('Bad Request')
+
+                
 
             context = {
                     'string': return_string(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], params['public']),
