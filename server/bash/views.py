f4de4a274da65c7cbb680ef90be5f14c4b6739f7
Oriol López-Doriga <orildsaga@gmail.com>
2023-08-18 12:49:12 +0200

    breaking the loop

--- server/bash/views.py
+++ server/bash/views.py
@@ -279,20 +279,21 @@ def return_bash(value1, value2, value3, value4, value5, value6, value7, value8):
     bash_list = bash.split(b'\n')
 
     new_bash_list=[]
-
-    item = bash_list[0].decode("utf-8") 
-    item = bash_list[0].replace('[', '')
-    item = bash_list[0].replace(']', '')
-    item = bash_list[0].replace('(', '')
-    item = bash_list[0].replace(')', '')
-    item = bash_list[0].replace(' ', '')
-    print("item is: {}".format(item))
-    item_list = item.split(',')
-    print("item_list is: {}".format(item_list))
-    boolean = item_list[0]
-    print("boolean: {}". format(boolean))
-    num_results = item_list[1]
-    datasets_list = item_list[2:-1]
+    for item in bash_list:
+        item = item.decode("utf-8") 
+        item = item.replace('[', '')
+        item = item.replace(']', '')
+        item = item.replace('(', '')
+        item = item.replace(')', '')
+        item = item.replace(' ', '')
+        print("item is: {}".format(item))
+        item_list = item.split(',')
+        print("item_list is: {}".format(item_list))
+        boolean = item_list[0]
+        print("boolean: {}". format(boolean))
+        num_results = item_list[1]
+        datasets_list = item_list[2:-1]
+        break
     
     new_bash_list.append(boolean)
     new_bash_list.append(num_results)
