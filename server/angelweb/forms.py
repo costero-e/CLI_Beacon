462bda68f4aedc398a219145b6480755531beb79
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-06 17:23:44 +0200

    initial true

--- server/angelweb/forms.py
+++ server/angelweb/forms.py
@@ -17,14 +17,14 @@ class BamFormTrue(forms.Form):
     def __init__(self, *args, **kwargs):
         super(BamFormTrue, self).__init__(*args, **kwargs)
         # assign a (computed, I assume) default value to the choice field
-        self.initial['reference'] = '38'
+        self.initial['reference'] = '37'
     choices_References = [(str(x), "GRCh" + str(x)) for x in range(37, 39)]
     choices = [(str(x), x) for x in range(1, 23)] + [("X", "X"), ("Y", "Y"), ("MT", "MT")]
     answer_choices = [("BOOL", "BOOL"), ("COUNT", "COUNT")]
     reference = forms.ChoiceField(choices=choices_References, help_text="<span class='hovertext' data-hover='Name of the genome to be queried'>Reference</span>", label="")
     chromosome = forms.ChoiceField(choices=choices, initial=[str(1),1],help_text="<span class='hovertext' data-hover='Name of the chromosome to be queried'>Chromosome</span>", label="")
-    start = forms.IntegerField(min_value=0, max_value=99999999999, initial=20210 ,help_text="<span class='hovertext' data-hover='Position from which query will start looking at'>Start</span>", label="")
-    region = forms.IntegerField(min_value=0, max_value=10000, required=False,help_text="<span class='hovertext' data-hover='Length of the query in genomic variants'>Region</span>", label="")
+    start = forms.IntegerField(min_value=0, max_value=99999999999, initial=13000000 ,help_text="<span class='hovertext' data-hover='Position from which query will start looking at'>Start</span>", label="")
+    region = forms.IntegerField(min_value=0, max_value=10000, initial=10000, required=False,help_text="<span class='hovertext' data-hover='Length of the query in genomic variants'>Region</span>", label="")
     mutated_allele = forms.CharField(max_length=50, required=False, help_text="<span class='hovertext' data-hover='Search for a specific variation that query will look for'>Mutated Allele</span>", label="")
     answer_type = forms.ChoiceField(choices=answer_choices, help_text="<span class='hovertext' data-hover='BOOL for a yes/no, COUNT for number of results'>Answer type</span>", label="")
     liftover = forms.BooleanField(required=False, help_text="<span class='hovertext' data-hover='Liftover'>Liftover</span>", label="")
