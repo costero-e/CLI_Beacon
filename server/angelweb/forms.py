cffc8a9affd2e83687e1fd1d39c28d93f2c4d43f
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-06 16:26:02 +0200

    adding field value

--- server/angelweb/forms.py
+++ server/angelweb/forms.py
@@ -2,18 +2,6 @@ from django import forms
 from django.core.exceptions import ValidationError
 
 class BamForm(forms.Form):
-    def clean(self):
-        region = self.cleaned_data.get('region')
-
-        if region > 0:
-            # Here we're raising a ValidationError that refers to a specific
-            # field so the error is better pointed out to the user.
-            raise ValidationError("You can't fill mutated allele if region is filled")
-
-        return self.cleaned_data
-            
-
-            
     choices_References = [(str(x), "GRCh" + str(x)) for x in range(37, 39)]
     choices = [(str(x), x) for x in range(1, 23)] + [("X", "X"), ("Y", "Y"), ("MT", "MT")]
     answer_choices = [("BOOL", "BOOL"), ("COUNT", "COUNT")]
@@ -25,6 +13,22 @@ class BamForm(forms.Form):
     answer_type = forms.ChoiceField(choices=answer_choices, help_text="<span class='hovertext' data-hover='BOOL for a yes/no, COUNT for number of results'>Answer type</span>", label="")
     liftover = forms.BooleanField(required=False, help_text="<span class='hovertext' data-hover='Liftover'>Liftover</span>", label="")
     public = forms.BooleanField(required=False, help_text="<span class='hovertext' data-hover='Public'>Public</span>", label="")
+    def clean(self):
+        region = self.cleaned_data.get('region')
+
+        if region > 0:
+            # Here we're raising a ValidationError that refers to a specific
+            # field so the error is better pointed out to the user.
+            raise ValidationError({
+                'region': ValidationError(
+                    'If you fill in available_from field, you also need to '
+                    'fill out available_till.', 
+                    code='required'
+                )
+            })
+
+        return self.cleaned_data
+            
 
 
 class BamFormTrue(forms.Form):
