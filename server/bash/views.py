156ed003cfd847316d09f17cf84e83a40b35eb4d
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-17 19:01:35 +0200

    improving code

--- server/bash/views.py
+++ server/bash/views.py
@@ -170,251 +170,6 @@ def return_list(value1, value2, value3, value4, value5, value6, value7):
 
     return string_list
 
-def return_boolean(value1, value2, value3, value4, value5, value6, value7, current_email):
-    if isinstance(value1, list):
-        for value in value1:
-            value1 = str(value)
-            value1 = "'" + value1 + "'"
-    elif isinstance(value1, int):
-        value = str(value)
-        value1 = value
-        value1 = "'" + value1 + "'"
-    else:
-        value1 = str(value1)
-        value1 = "'" + value1 + "'"
-    if isinstance(value2, list):
-        for value in value2:
-            value2 = str(value)
-            value2 = "'" + value2 + "'"
-    elif isinstance(value2, int):
-        value = str(value)
-        value2 = value
-        value2 = "'" + value2 + "'"
-    else:
-        value2 = str(value2)
-        value2 = "'" + value2 + "'"
-    if isinstance(value3, list):
-        for value in value3:
-            value3 = str(value)
-            value3 = "'" + value3 + "'"
-    elif isinstance(value3, int):
-        value = str(value3)
-        value3 = value
-        value3 = "'" + value3 + "'"
-    else:
-        value3 = str(value3)
-        value3 = "'" + value3 + "'"
-    if isinstance(value4, list):
-        for value in value4:
-            value4 = str(value)
-            value4 = "'" + value4 + "'"
-    elif isinstance(value4, int):
-        value = str(value4)
-        value4 = value
-        value4 = "'" + value4 + "'"
-    elif value4 == None:
-        value4 = ''
-        value4 = "'" + value4 + "'"
-    if isinstance(value5, list):
-        for value in value5:
-            value5 = str(value)
-            value5 = "'" + value5 + "'"
-    elif isinstance(value5, int):
-        value = str(value)
-        value5 = value
-        value5 = "'" + value5 + "'"
-    else:
-        value5 = str(value5)
-        value5 = "'" + value5 + "'"
-    if isinstance(value7, list):
-        for value in value7:
-            value7 = str(value)
-            value7 = "'" + value7 + "'"
-    elif isinstance(value7, int):
-        value = str(value)
-        value7 = value
-        value7 = "'" + value7 + "'"
-    else:
-        value7 = str(value7)
-        value7 = "'" + value7 + "'"
-
-    for value in value6:
-        if value == 'True':
-            value6 = 'LIFTOVER'
-            value6 = "'" + value6 + "'"
-        else:
-            value6 = ''
-            value6 = "'" + value6 + "'"
-
-    string = ' --chr' + ' ' + value2 + ' --pos' + ' ' + value3 + ' --ref_gen' + ' ' + value1
-
-    if current_email:
-        string = string +  ' --email' + ' ' + str(current_email)
-    if value6 == "'LIFTOVER'":
-        string = string + ' --liftover'
-
-    print(string)
-    if value4 != "''":
-        string = string + ' -w' + ' ' + value4 
-    if value5 != "''":
-        string = string + ' -a' + ' ' + value5
-    
-    bash_string = 'cd /beacon-BED-based/ ' + '&&' + ' python3 main.py' + string
-
-    try:
-        bash = subprocess.check_output([bash_string], shell=True)
-    except subprocess.CalledProcessError as e:
-        output = e.output
-        print(output)
-
-    bash_list = bash.split(b'\n')
-
-    new_bash_list=[]
-    for item in bash_list:
-        item = item.decode("utf-8") 
-        item = item.replace('[', '')
-        item = item.replace(']', '')
-        item = item.replace('(', '')
-        item = item.replace(')', '')
-        item = item.replace(' ', '')
-        item = item.replace("'", '')
-        item = item.replace('"', '')
-        print("item is: {}".format(item))
-        item_list = item.split(',')
-        print("item_list is: {}".format(item_list))
-        boolean = item_list[0]
-        print("boolean: {}". format(boolean))
-        num_results = item_list[1]
-        end = len(item_list)
-        datasets_list = item_list[2:end]
-
-        break
-
-    return boolean
-
-def return_num_results(value1, value2, value3, value4, value5, value6, value7, current_email):
-    if isinstance(value1, list):
-        for value in value1:
-            value1 = str(value)
-            value1 = "'" + value1 + "'"
-    elif isinstance(value1, int):
-        value = str(value)
-        value1 = value
-        value1 = "'" + value1 + "'"
-    else:
-        value1 = str(value1)
-        value1 = "'" + value1 + "'"
-    if isinstance(value2, list):
-        for value in value2:
-            value2 = str(value)
-            value2 = "'" + value2 + "'"
-    elif isinstance(value2, int):
-        value = str(value)
-        value2 = value
-        value2 = "'" + value2 + "'"
-    else:
-        value2 = str(value2)
-        value2 = "'" + value2 + "'"
-    if isinstance(value3, list):
-        for value in value3:
-            value3 = str(value)
-            value3 = "'" + value3 + "'"
-    elif isinstance(value3, int):
-        value = str(value3)
-        value3 = value
-        value3 = "'" + value3 + "'"
-    else:
-        value3 = str(value3)
-        value3 = "'" + value3 + "'"
-    if isinstance(value4, list):
-        for value in value4:
-            value4 = str(value)
-            value4 = "'" + value4 + "'"
-    elif isinstance(value4, int):
-        value = str(value4)
-        value4 = value
-        value4 = "'" + value4 + "'"
-    elif value4 == None:
-        value4 = ''
-        value4 = "'" + value4 + "'"
-    if isinstance(value5, list):
-        for value in value5:
-            value5 = str(value)
-            value5 = "'" + value5 + "'"
-    elif isinstance(value5, int):
-        value = str(value)
-        value5 = value
-        value5 = "'" + value5 + "'"
-    else:
-        value5 = str(value5)
-        value5 = "'" + value5 + "'"
-    if isinstance(value7, list):
-        for value in value7:
-            value7 = str(value)
-            value7 = "'" + value7 + "'"
-    elif isinstance(value7, int):
-        value = str(value)
-        value7 = value
-        value7 = "'" + value7 + "'"
-    else:
-        value7 = str(value7)
-        value7 = "'" + value7 + "'"
-
-    for value in value6:
-        if value == 'True':
-            value6 = 'LIFTOVER'
-            value6 = "'" + value6 + "'"
-        else:
-            value6 = ''
-            value6 = "'" + value6 + "'"
-
-    string = ' --chr' + ' ' + value2 + ' --pos' + ' ' + value3 + ' --ref_gen' + ' ' + value1
-
-    if current_email:
-        string = string +  ' --email' + ' ' + str(current_email)
-    if value6 == "'LIFTOVER'":
-        string = string + ' --liftover'
-
-    print(string)
-    if value4 != "''":
-        string = string + ' -w' + ' ' + value4 
-    if value5 != "''":
-        string = string + ' -a' + ' ' + value5
-    
-    bash_string = 'cd /beacon-BED-based/ ' + '&&' + ' python3 main.py' + string
-
-    try:
-        bash = subprocess.check_output([bash_string], shell=True)
-    except subprocess.CalledProcessError as e:
-        output = e.output
-        print(output)
-
-    bash_list = bash.split(b'\n')
-
-    new_bash_list=[]
-    for item in bash_list:
-        item = item.decode("utf-8") 
-        item = item.replace('[', '')
-        item = item.replace(']', '')
-        item = item.replace('(', '')
-        item = item.replace(')', '')
-        item = item.replace(' ', '')
-        item = item.replace("'", '')
-        item = item.replace('"', '')
-        print("item is: {}".format(item))
-        item_list = item.split(',')
-        print("item_list is: {}".format(item_list))
-        boolean = item_list[0]
-        print("boolean: {}". format(boolean))
-        num_results = item_list[1]
-        end = len(item_list)
-        datasets_list = item_list[2:end]
-
-        break
-    
-
-    return num_results
-
 def return_datasets(value1, value2, value3, value4, value5, value6, value7, current_email):
     if isinstance(value1, list):
         for value in value1:
@@ -534,7 +289,8 @@ def return_datasets(value1, value2, value3, value4, value5, value6, value7, curr
         datasets_list = item_list[2:end]
 
         break
-
+    
+    num_datasets = len(datasets_list)
     dataset_dict={}
     last_element=''
     for element in datasets_list:
@@ -545,139 +301,15 @@ def return_datasets(value1, value2, value3, value4, value5, value6, value7, curr
             last_element=element
     print("datasets_dict: {}". format(dataset_dict))
 
+    dataset_dict["num_datasets"] = num_datasets
+    dataset_dict["num_results"] = num_results
+    dataset_dict["boolean"] = boolean
 
 
     
 
     return dataset_dict
 
-def return_num_datasets(value1, value2, value3, value4, value5, value6, value7, current_email):
-    if isinstance(value1, list):
-        for value in value1:
-            value1 = str(value)
-            value1 = "'" + value1 + "'"
-    elif isinstance(value1, int):
-        value = str(value)
-        value1 = value
-        value1 = "'" + value1 + "'"
-    else:
-        value1 = str(value1)
-        value1 = "'" + value1 + "'"
-    if isinstance(value2, list):
-        for value in value2:
-            value2 = str(value)
-            value2 = "'" + value2 + "'"
-    elif isinstance(value2, int):
-        value = str(value)
-        value2 = value
-        value2 = "'" + value2 + "'"
-    else:
-        value2 = str(value2)
-        value2 = "'" + value2 + "'"
-    if isinstance(value3, list):
-        for value in value3:
-            value3 = str(value)
-            value3 = "'" + value3 + "'"
-    elif isinstance(value3, int):
-        value = str(value3)
-        value3 = value
-        value3 = "'" + value3 + "'"
-    else:
-        value3 = str(value3)
-        value3 = "'" + value3 + "'"
-    if isinstance(value4, list):
-        for value in value4:
-            value4 = str(value)
-            value4 = "'" + value4 + "'"
-    elif isinstance(value4, int):
-        value = str(value4)
-        value4 = value
-        value4 = "'" + value4 + "'"
-    elif value4 == None:
-        value4 = ''
-        value4 = "'" + value4 + "'"
-    if isinstance(value5, list):
-        for value in value5:
-            value5 = str(value)
-            value5 = "'" + value5 + "'"
-    elif isinstance(value5, int):
-        value = str(value)
-        value5 = value
-        value5 = "'" + value5 + "'"
-    else:
-        value5 = str(value5)
-        value5 = "'" + value5 + "'"
-    if isinstance(value7, list):
-        for value in value7:
-            value7 = str(value)
-            value7 = "'" + value7 + "'"
-    elif isinstance(value7, int):
-        value = str(value)
-        value7 = value
-        value7 = "'" + value7 + "'"
-    else:
-        value7 = str(value7)
-        value7 = "'" + value7 + "'"
-
-    for value in value6:
-        if value == 'True':
-            value6 = 'LIFTOVER'
-            value6 = "'" + value6 + "'"
-        else:
-            value6 = ''
-            value6 = "'" + value6 + "'"
-
-    string = ' --chr' + ' ' + value2 + ' --pos' + ' ' + value3 + ' --ref_gen' + ' ' + value1
-
-    if current_email:
-        string = string +  ' --email' + ' ' + str(current_email)
-    if value6 == "'LIFTOVER'":
-        string = string + ' --liftover'
-
-    print(string)
-    if value4 != "''":
-        string = string + ' -w' + ' ' + value4 
-    if value5 != "''":
-        string = string + ' -a' + ' ' + value5
-    
-    bash_string = 'cd /beacon-BED-based/ ' + '&&' + ' python3 main.py' + string
-
-    try:
-        bash = subprocess.check_output([bash_string], shell=True)
-    except subprocess.CalledProcessError as e:
-        output = e.output
-        print(output)
-
-    bash_list = bash.split(b'\n')
-
-    new_bash_list=[]
-    for item in bash_list:
-        item = item.decode("utf-8") 
-        item = item.replace('[', '')
-        item = item.replace(']', '')
-        item = item.replace('(', '')
-        item = item.replace(')', '')
-        item = item.replace(' ', '')
-        item = item.replace("'", '')
-        item = item.replace('"', '')
-        print("item is: {}".format(item))
-        item_list = item.split(',')
-        print("item_list is: {}".format(item_list))
-        boolean = item_list[0]
-        print("boolean: {}". format(boolean))
-        num_results = item_list[1]
-        end = len(item_list)
-        datasets_list = item_list[2:end]
-
-        break
-
-    num_datasets = len(datasets_list)
-
-
-
-    
-
-    return num_datasets
 
 
 def bash_view(request):
@@ -781,12 +413,21 @@ def bash_view(request):
             print(current_email)
                 
 
+            dict_complete = return_datasets(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email)
+            boolean = dict_complete["boolean"]
+            del dict_complete["boolean"]
+            num_results = dict_complete["num_results"]
+            del dict_complete["num_results"]
+            num_datasets = dict_complete["num_datasets"]
+            del dict_complete["num_datasets"]
+
+
             context = {
                     'string': return_string(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email),
-                    'boolean': return_boolean(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email),
-                    'num_results': return_num_results(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email),
-                    'datasets': return_datasets(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email),
-                    'num_datasets': return_num_datasets(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email),
+                    'boolean': boolean,
+                    'num_results': num_results,
+                    'datasets': dict_complete,
+                    'num_datasets': num_datasets,
                     'form': form
 
 
@@ -901,13 +542,21 @@ def bash_true_view(request):
 
                 return HttpResponseBadRequest('Bad Request')
                 
+            dict_complete = return_datasets(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email)
+            boolean = dict_complete["boolean"]
+            del dict_complete["boolean"]
+            num_results = dict_complete["num_results"]
+            del dict_complete["num_results"]
+            num_datasets = dict_complete["num_datasets"]
+            del dict_complete["num_datasets"]
+
 
             context = {
                     'string': return_string(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email),
-                    'boolean': return_boolean(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email),
-                    'num_results': return_num_results(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email),
-                    'datasets': return_datasets(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email),
-                    'num_datasets': return_num_datasets(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email),
+                    'boolean': boolean,
+                    'num_results': num_results,
+                    'datasets': dict_complete,
+                    'num_datasets': num_datasets,
                     'form': form
 
 
@@ -1023,12 +672,21 @@ def bash_false_view(request):
                 return HttpResponseBadRequest('Bad Request')
                 
 
+            dict_complete = return_datasets(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email)
+            boolean = dict_complete["boolean"]
+            del dict_complete["boolean"]
+            num_results = dict_complete["num_results"]
+            del dict_complete["num_results"]
+            num_datasets = dict_complete["num_datasets"]
+            del dict_complete["num_datasets"]
+
+
             context = {
                     'string': return_string(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email),
-                    'boolean': return_boolean(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email),
-                    'num_results': return_num_results(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email),
-                    'datasets': return_datasets(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email),
-                    'num_datasets': return_num_datasets(params['reference'], params['chromosome'], params['start'], params['region'], params['mutated_allele'], params['liftover'], params['answer_type'], current_email),
+                    'boolean': boolean,
+                    'num_results': num_results,
+                    'datasets': dict_complete,
+                    'num_datasets': num_datasets,
                     'form': form
 
 
