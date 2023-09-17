7720083adaef99fc0e3e031989721173f5d9bc7f
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-17 19:51:24 +0200

    adding javascript

--- server/media/form.js
+++ server/media/form.js
@@ -1,11 +1,9 @@
- function makeReadOnly()
- {        
-  if (document.getElementById('id_region').value != ''){
+
+if (document.getElementById('id_region').value != ''){
       document.getElementById('id_mutated_allele').readOnly=true;
  }else if (document.getElementById('id_region').value != '0'){ 
       document.getElementById('id_mutated_allele').readOnly=true;
   } else { 
     document.getElementById('id_mutated_allele').readOnly=false;
 }
- }
- document.getElementById('id_region').addEventListener('change', makeReadOnly);
+
