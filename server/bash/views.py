b3b706a5869457396a27c9b426705a52542f884d
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-19 16:07:48 +0200

    debugging keeping values

--- server/bash/views.py
+++ server/bash/views.py
@@ -346,7 +346,7 @@ def bash_view(request):
                 get_string='?reference={}&chromosome={}&start={}&mutated_allele={}&liftover={}&public={}'.format(form.cleaned_data['reference'], form.cleaned_data['chromosome'], form.cleaned_data['start'], form.cleaned_data['mutated_allele'], form.cleaned_data['liftover'], form.cleaned_data['public'])
             else:
                 get_string='?reference={}&chromosome={}&start={}&mutated_allele={}&liftover={}&public={}'.format(form.cleaned_data['reference'], form.cleaned_data['chromosome'], form.cleaned_data['start'], form.cleaned_data['mutated_allele'], form.cleaned_data['liftover'], form.cleaned_data['public'])
-
+            form = BamForm(initial={'start': form.cleaned_data['start']})
             return HttpResponseRedirect('/' + get_string)
         else:
            raise TypeError("can't fill mutated allele if region is specified")
