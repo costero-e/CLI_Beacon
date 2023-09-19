a6fff61799bd863da86c76b8cab04f4ab931ac8b
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-19 14:46:16 +0200

    adding keeping values

--- server/angelweb/models.py
+++ server/angelweb/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 
-class BamModel(models.model):
-    def __init__(self, *args, **kwargs):
-        super(BamForm, self).__init__(*args, **kwargs)
-        # assign a (computed, I assume) default value to the choice field
-        self.initial['public'] = True  
-    def clean(self):
-        cleaned_data = super(BamForm, self).clean()
-        region = cleaned_data.get('region', None)
-        mutated_allele = cleaned_data.get('mutated_allele', None)
-        if mutated_allele and region!=0:
-            raise models.ValidationError("can't fill mutated allele if region is specified")
+class BamModel(models.Model):
     choices_References = [(str(x), "GRCh" + str(x)) for x in range(37, 39)]
     choices = [(str(x), x) for x in range(1, 23)] + [("X", "X"), ("Y", "Y"), ("MT", "MT")]
     reference = models.ChoiceField(choices=choices_References, help_text="<span class='hovertext' data-hover='Name of the genome to be queried'>Reference</span>", label="")
