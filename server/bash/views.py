629a550f27ad9724ed7ec2e6cadc060c76399f20
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-19 17:37:43 +0200

    adding mutated allele condition

--- server/bash/views.py
+++ server/bash/views.py
@@ -348,8 +348,7 @@ def bash_view(request):
                 get_string='?reference={}&chromosome={}&start={}&mutated_allele={}&liftover={}&public={}'.format(form.cleaned_data['reference'], form.cleaned_data['chromosome'], form.cleaned_data['start'], form.cleaned_data['mutated_allele'], form.cleaned_data['liftover'], form.cleaned_data['public'])
 
             return HttpResponseRedirect('/' + get_string)
-        else:
-           raise form.add_error(None, "can't fill mutated allele if region is specified")
+
         
     if request.method == 'GET':
         print('getting')
