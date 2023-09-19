7ca3bbf5768364bccc0b6f4e57f41cb956632939
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-19 14:32:41 +0200

    adding keeping values

--- server/angelweb/forms.py
+++ server/angelweb/forms.py
@@ -20,6 +20,10 @@ class BamForm(forms.Form):
     mutated_allele = forms.CharField(max_length=50, required=False, help_text="<span class='hovertext' data-hover='Search for a specific variation that query will look for'>Mutated Allele</span>", label="")
     liftover = forms.BooleanField(required=False, help_text="<span class='hovertext' data-hover='Liftover'>Liftover</span>", label="")
     public = forms.BooleanField(required=False, help_text="<span class='hovertext' data-hover='Public'>Public</span>", label="")
+
+class BamModelForm(forms.ModelForm):
+    class Meta:
+        model = BamForm
     
 
 class BamFormTrue(forms.Form):
