42175901b3939b6dbb7aae4a1fcb0f91bf1fe303
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-19 16:59:51 +0200

    debugging keeping values

--- server/media/submit.js
+++ server/media/submit.js
@@ -1,3 +1,9 @@
-var btn = document.getElementById('id_start');
-if (btn) {console.log(btn.value)}
+var nameInput = document.getElementById('id_start');
 
+document.querySelector('form.superform').addEventListener('submit', function (e) {
+
+    //prevent the normal submission of the form
+    e.preventDefault();
+
+    console.log(nameInput.value);    
+});
