324fdda6c307601c1af7e8fb45f7392626efd627
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-19 15:18:15 +0200

    debugging keeping values

--- server/angelweb/forms.py
+++ server/angelweb/forms.py
@@ -5,25 +5,14 @@ class BamForm(forms.Form):
         super(BamForm, self).__init__(*args, **kwargs)
         # assign a (computed, I assume) default value to the choice field
         self.initial['public'] = True  
+        if self.instance:
+            self.fields['start'].initial = self.instance.start
     def clean(self):
         cleaned_data = super(BamForm, self).clean()
-        reference = cleaned_data.get('reference', None)
-        self.initial['reference'] = reference 
-        chromosome = cleaned_data.get('chromosome', None)
-        self.initial['chromosome'] = chromosome 
-        start = cleaned_data.get('start', None)
-        self.initial['start'] = start 
         region = cleaned_data.get('region', None)
-        self.initial['region'] = region 
         mutated_allele = cleaned_data.get('mutated_allele', None)
-        self.initial['mutated_allele'] = mutated_allele 
-        liftover = cleaned_data.get('liftover', None)
-        self.initial['liftover'] = liftover 
-        public = cleaned_data.get('public', None)
-        self.initial['public'] = public 
         if mutated_allele and region!=0:
             raise forms.ValidationError("can't fill mutated allele if region is specified")
-        
     choices_References = [(str(x), "GRCh" + str(x)) for x in range(37, 39)]
     choices = [(str(x), x) for x in range(1, 23)] + [("X", "X"), ("Y", "Y"), ("MT", "MT")]
     reference = forms.ChoiceField(choices=choices_References, help_text="<span class='hovertext' data-hover='Name of the genome to be queried'>Reference</span>", label="")
