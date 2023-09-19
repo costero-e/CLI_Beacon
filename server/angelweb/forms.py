32b90164622d0491a256450a32cd64c6d7064243
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-19 15:37:35 +0200

    debugging keeping values

--- server/angelweb/forms.py
+++ server/angelweb/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from .models import BamModel
 
-class BamForm(forms.Form):
+class BamForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(BamForm, self).__init__(*args, **kwargs)
         # assign a (computed, I assume) default value to the choice field
@@ -11,17 +12,11 @@ class BamForm(forms.Form):
         mutated_allele = cleaned_data.get('mutated_allele', None)
         if mutated_allele and region!=0:
             raise forms.ValidationError("can't fill mutated allele if region is specified")
-        return cleaned_data
+    class Meta:
+            model = BamModel
+            fields = ['reference', 'chromosome','start','region', 'mutated_allele', 'liftover', 'public']
 
-    choices_References = [(str(x), "GRCh" + str(x)) for x in range(37, 39)]
-    choices = [(str(x), x) for x in range(1, 23)] + [("X", "X"), ("Y", "Y"), ("MT", "MT")]
-    reference = forms.ChoiceField(choices=choices_References, help_text="<span class='hovertext' data-hover='Name of the genome to be queried'>Reference</span>", label="")
-    chromosome = forms.ChoiceField(choices=choices, help_text="<span class='hovertext' data-hover='Name of the chromosome to be queried'>Chromosome</span>", label="")
-    start = forms.IntegerField(min_value=0, max_value=99999999999, help_text="<span class='hovertext' data-hover='Position from which query will start looking at'>Start</span>", label="")
-    region = forms.IntegerField(min_value=0, max_value=10000, required=False, help_text="<span class='hovertext' data-hover='Length of the query in genomic variants'>Region</span>", label="")
-    mutated_allele = forms.CharField(max_length=50, required=False, help_text="<span class='hovertext' data-hover='Search for a specific variation that query will look for'>Mutated Allele</span>", label="")
-    liftover = forms.BooleanField(required=False, help_text="<span class='hovertext' data-hover='Liftover'>Liftover</span>", label="")
-    public = forms.BooleanField(required=False, help_text="<span class='hovertext' data-hover='Public'>Public</span>", label="")
+
 
 class BamFormTrue(forms.Form):
     def __init__(self, *args, **kwargs):
