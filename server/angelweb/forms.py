73c89518094c14de0e3a2131cc2f26c687fd066f
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-19 17:19:34 +0200

    adding mutated allele condition

--- server/angelweb/forms.py
+++ server/angelweb/forms.py
@@ -11,7 +11,7 @@ class BamForm(forms.Form):
         region = cleaned_data.get('region', None)
         mutated_allele = cleaned_data.get('mutated_allele', None)
         if mutated_allele and region!=0:
-            raise forms.ValidationError("can't fill mutated allele if region is specified")
+            self.add_error('mutated_allele','You can not specify this field if region is specified')
     
     choices_References = [(str(x), "GRCh" + str(x)) for x in range(37, 39)]
     choices = [(str(x), x) for x in range(1, 23)] + [("X", "X"), ("Y", "Y"), ("MT", "MT")]
--- server/media/submit.js
+++
@@ -1,9 +0,0 @@
-var nameInput = document.getElementById('id_start');
-
-document.querySelector('form.superform').addEventListener('submit', function (e) {
-
-    var newValue = nameInput.value;
-    console.log(newValue);
-    document.getElementById('id_start').value = newValue;   
-});
-
