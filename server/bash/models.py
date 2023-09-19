3f3a352ecb8a6848e2e483ce6896ddacd133d84a
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-19 16:24:35 +0200

    debugging keeping values

--- server/bash/models.py
+++ server/bash/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class BamModel(models.Model):
     start = models.IntegerField()
     region = models.IntegerField()
-    mutated_allele = models.CharField()
+    mutated_allele = models.CharField(max_length=50)
     liftover = models.BooleanField()
     public = models.BooleanField()
