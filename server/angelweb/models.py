bf1fb7bd323b62bed835eb6683046da5d9f8042a
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-19 16:22:04 +0200

    debugging keeping values

--- server/angelweb/models.py
+++ server/angelweb/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class BamModel(models.Model):
-    start = models.IntegerField(min_value=0, max_value=99999999999, help_text="<span class='hovertext' data-hover='Position from which query will start looking at'>Start</span>", label="")
-    region = models.IntegerField(min_value=0, max_value=10000, required=False, help_text="<span class='hovertext' data-hover='Length of the query in genomic variants'>Region</span>", label="")
-    mutated_allele = models.CharField(max_length=50, required=False, help_text="<span class='hovertext' data-hover='Search for a specific variation that query will look for'>Mutated Allele</span>", label="")
-    liftover = models.BooleanField(required=False, help_text="<span class='hovertext' data-hover='Liftover'>Liftover</span>", label="")
-    public = models.BooleanField(required=False, help_text="<span class='hovertext' data-hover='Public'>Public</span>", label="")
+    start = models.IntegerField()
+    region = models.IntegerField()
+    mutated_allele = models.CharField()
+    liftover = models.BooleanField()
+    public = models.BooleanField()
