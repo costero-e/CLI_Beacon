54e1152ca1aefd1ac774babd99f7e91fcf7c1eaa
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-20 14:00:05 +0200

    trying to pass initial values

--- server/media/saving.js
+++ server/media/saving.js
@@ -3,4 +3,5 @@ var input = document.getElementById("id_region");
 input.replace('[','')
 input.replace("'",'')
 let b = parseInt(input);
+console.log(b)
 localStorage.setItem("id_region", b.val());}
