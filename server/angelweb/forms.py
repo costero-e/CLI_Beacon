c985156f060f15d4faf7513b106d771f44fffffc
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-20 09:56:43 +0200

    changing example

--- server/angelweb/forms.py
+++ server/angelweb/forms.py
@@ -38,7 +38,7 @@ class BamFormTrue(forms.Form):
     reference = forms.ChoiceField(choices=choices_References, help_text="<span class='hovertext' data-hover='Name of the genome to be queried'>Reference</span>", label="")
     chromosome = forms.ChoiceField(choices=choices, initial=[str(7),7],help_text="<span class='hovertext' data-hover='Name of the chromosome to be queried'>Chromosome</span>", label="")
     start = forms.IntegerField(min_value=0, max_value=99999999999, initial=140453136 ,help_text="<span class='hovertext' data-hover='Position from which query will start looking at'>Start</span>", label="")
-    region = forms.IntegerField(min_value=0, max_value=10000, initial=10000, required=False,help_text="<span class='hovertext' data-hover='Length of the query in genomic variants'>Region</span>", label="")
+    region = forms.IntegerField(min_value=0, max_value=10000, required=False,help_text="<span class='hovertext' data-hover='Length of the query in genomic variants'>Region</span>", label="")
     mutated_allele = forms.CharField(max_length=50, required=False, help_text="<span class='hovertext' data-hover='Search for a specific variation that query will look for'>Mutated Allele</span>", label="")
     liftover = forms.BooleanField(required=False, help_text="<span class='hovertext' data-hover='Liftover'>Liftover</span>", label="")
     public = forms.BooleanField(required=False, help_text="<span class='hovertext' data-hover='Public'>Public</span>", label="")
