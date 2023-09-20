e0aab0e115d6bd29ebf9e959c5bdc15c919e2c77
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-20 13:57:17 +0200

    trying to pass initial values

--- server/media/loading.js
+++ server/media/loading.js
@@ -1,6 +1,9 @@
 
 var storedValue = localStorage.getItem("id_region");
+storedValue.replace('[','')
+storedValue.replace("'",'')
+let b = parseInt(storedValue);
 
 window.onload = function(){
-  document.getElementById("id_region").value = storedValue;
+  document.getElementById("id_region").value = b;
 }
