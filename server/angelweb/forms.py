1b48b022e5819ffc10dc8ddfd9c739af2e7e3e6a
Oriol López-Doriga <orildsaga@gmail.com>
2023-09-20 09:49:19 +0200

    changing example

--- server/angelweb/forms.py
+++ server/angelweb/forms.py
@@ -36,8 +36,8 @@ class BamFormTrue(forms.Form):
     choices_References = [(str(x), "GRCh" + str(x)) for x in range(37, 39)]
     choices = [(str(x), x) for x in range(1, 23)] + [("X", "X"), ("Y", "Y"), ("MT", "MT")]
     reference = forms.ChoiceField(choices=choices_References, help_text="<span class='hovertext' data-hover='Name of the genome to be queried'>Reference</span>", label="")
-    chromosome = forms.ChoiceField(choices=choices, initial=[str(1),1],help_text="<span class='hovertext' data-hover='Name of the chromosome to be queried'>Chromosome</span>", label="")
-    start = forms.IntegerField(min_value=0, max_value=99999999999, initial=13000000 ,help_text="<span class='hovertext' data-hover='Position from which query will start looking at'>Start</span>", label="")
+    chromosome = forms.ChoiceField(choices=choices, initial=[str(7),7],help_text="<span class='hovertext' data-hover='Name of the chromosome to be queried'>Chromosome</span>", label="")
+    start = forms.IntegerField(min_value=0, max_value=99999999999, initial=140453136 ,help_text="<span class='hovertext' data-hover='Position from which query will start looking at'>Start</span>", label="")
     region = forms.IntegerField(min_value=0, max_value=10000, initial=10000, required=False,help_text="<span class='hovertext' data-hover='Length of the query in genomic variants'>Region</span>", label="")
     mutated_allele = forms.CharField(max_length=50, required=False, help_text="<span class='hovertext' data-hover='Search for a specific variation that query will look for'>Mutated Allele</span>", label="")
     liftover = forms.BooleanField(required=False, help_text="<span class='hovertext' data-hover='Liftover'>Liftover</span>", label="")
